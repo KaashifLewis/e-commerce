476dc1f6df34bf7d6adacedcceec54678aa261b2
Kaashif lewis <kaashif21@hotmail.com>
2026-02-25 20:32:40 -0400

    Implemented checkout functionality

--- app/Http/Controllers/CheckoutPaymentController.php
+++ app/Http/Controllers/CheckoutPaymentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Order;
 use App\Helpers\ShippingHelper;
 use App\Helpers\StripeCheckout;
+use App\Models\OrderProduct;
 use Illuminate\Support\Facades\Auth;
 
 
@@ -20,10 +21,10 @@ class CheckoutPaymentController extends Controller
      */
     public function index($payment)
     {
-        // Get groups
+        // This checks if the user is logged in and determine what group this user belongs to.
         $group_ids = Auth::check() ? Auth::user()->getGroups() : [1];
 
-        // Get user
+        // This function gets the user who is logged in.
         $user = Auth::user();
 
         // Create variables
@@ -33,21 +34,35 @@ class CheckoutPaymentController extends Controller
         $insert_data = [];
         $completed = false;
 
-        // Get products
+        // This function gets all the products the user added to the cart.
         $cart_data = $user->products()->withPrices()->get();
 
-        // Check if cart is empty
+        // This function checks to see if user's cart is empty
         if ($cart_data->isEmpty()) {
-            return redirect()->route('cart.index')->with('message', 'your cart is empty');
+            // displays a message to tell them its empty and redirect them back to cart page
+            return redirect()->route('cart.index')->with('message', 'Your cart is empty');
         }
 
-        // Get Subtotal
+        // The function displays the orginal cost of the item before deductions.
         $cart_data->calculateSubtotal();
 
         // Determine payment
         switch ($payment) {
             case 'stripe':
                 # code...
+
+
+                $stripe_checkout->startCheckoutSession();
+                $stripe_checkout->addEmail($user->email);
+                $stripe_checkout->addProducts($cart_data);
+                $stripe_checkout->addPointsCoupon();
+                $stripe_checkout->enablePromoCodes();
+                $shipping_data = $shipping_helper->getGroupShippingOptions();
+                $stripe_checkout->addShippingOptions($shipping_data);
+                $stripe_checkout->createSession();
+                $insert_data = $stripe_checkout->getOrderCreateData();
+                $completed = true;
+
                 break;
 
             default:
@@ -63,13 +78,44 @@ class CheckoutPaymentController extends Controller
         if (!$completed || empty($insert_data)) {
             dd('Payment is incomplete or checkout is missing');
         }
+        // Inserting values into the orders table
+        $order->user_id = $user->id;
+        $order->order_no = '123';
+        $order->subtotal = $cart_data->getSubtotal();
+        $order->total = $cart_data->getTotal();
+        $order->payment_provider = $insert_data['payment_provider'];
+        $order->payment_id = $insert_data['payment_id'];
+        $order->shipping_id = 1;
+        $order->shipping_address_id = 1;
+        $order->billing_address_id = 1;
+        $order->payment_status = 'unpaid';
+        $order->save();
 
-        // Create order details
 
-        // Create order details
+        // This is a loop that goes through each item in the cart
+        $records = [];
+
+        // This array
+        foreach ($cart_data as $data) {
+            array_push(
+                $records,
+                new OrderProduct([
+                    'product_id' => $data->id,
+                    'user_id' => $user->id,
+                    'price' => $data->getPrice(),
+                    'quantity' => $data->pivot->quantity
+                ])
+            );
+        }
+
+        // This line saves all the information and insert it into the order_products table
+        $order->order_products()->saveMany($records);
 
         // Redirect
+        if ($payment == 'stripe') {
+            return redirect($stripe_checkout->getUrl());
+        }
 
-        return true;
+        dd('Payment was successful during testing');
     }
 }
